3e032604b375f65263be4dc5960a73fba32c3505
Cameron <hackwithcameron@gmail.com>
2020-04-29 19:34:15 -0700

    Update pokedexAPI class
    Added a way to search with the set name and not set key. Started with models for sets and cards.

--- Card/models.py
+++ Card/models.py
@@ -1,5 +1,31 @@
 from django.db import models
 
 
+class CardSet(models.Model):
+    code = models.CharField(max_length=20)
+    ptcgo_code = models.CharField(max_length=20)
+    name = models.CharField(max_length=25)
+    series = models.CharField(max_length=25)
+    total_cards = models.IntegerField()
+    release_date = models.DateField(null=True)
+    symbol_url = models.URLField()
+    logo_url = models.URLField()
+
+
 class Card(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    national_pokedex_number = models.IntegerField()
+    image_url = models.URLField()
+    image_url_hi_res = models.URLField()
+    subtype = models.CharField(max_length=50)
+    supertype = models.CharField(max_length=50)
+    ancient_trait = models.CharField(max_length=50, null=True)
+    hp = models.CharField(max_length=10)
+    number = models.CharField(max_length=10)
+    artist = models.CharField(max_length=50)
+    rarity = models.CharField(max_length=50)
+    series = models.CharField(max_length=50)
+    card_set = models.ForeignKey(CardSet, on_delete=models.SET_NULL, null=True)
+    set_code = models.CharField(max_length=50)
+    types = models.CharField(max_length=20)
+    evolves_from = models.CharField(max_length=50)
--- Card/views.py
+++ Card/views.py
@@ -9,14 +9,12 @@ class Index(View):
 
     def get(self, request):
         pokedexAPI = PokedexAPI()
-        # Creates deck of cards
-        cards = [pokedexAPI.getCard('base1', f'{x}') for x in range(1, 17)]
-        # Creates single card
-        card = pokedexAPI.getCard('base2', '15')
+        card = pokedexAPI.getCard('base', 15)
+
+
 
         context = {
             'card': card,
-            'cards': cards
         }
 
         return render(request, 'Card/index.html', context)
